90a7b5cb73d2a8d3c86820c1619b2bb3603bde49
Pranjal Kukreja <pranjalkukreja086@gmail.com>
2021-01-12 18:24:36 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJZdGaoDIaYKQxMKRuK-05akhSCZMKfBg",
+    authDomain: "optima-mart.firebaseapp.com",
+    projectId: "optima-mart",
+    storageBucket: "optima-mart.appspot.com",
+    messagingSenderId: "1008403079021",
+    appId: "1:1008403079021:web:a67bd02c333f3748c20464",
+    measurementId: "G-CYJ9SW1W86"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export const auth = firebase.auth();
+//   export const googleAuthProvider = new firebase.auth.googleAuthProvider();
